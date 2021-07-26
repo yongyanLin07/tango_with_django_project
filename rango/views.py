8e036543a5420fff56580c7a0cec9a0c337554f0
Yongyan Lin <2529594L@student.gla.ac.uk>
2021-07-27 00:19:37 +0100

    chapter4 Adding a Template

--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request): 
-    url = "<a href='/rango/about'>About</a>"
-    return HttpResponse("Rango says hey there partner!"+url)
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    #chapter2 url = "<a href='/rango/about'>About</a>"
+    return render(request, 'rango/index.html', context=context_dict)
+
 
 def about(request):
     url = "<a href='/rango/'>Index</a>"
