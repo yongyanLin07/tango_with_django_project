5815ddcc2a473a1485c1ea65626a0bcd9bbe5504
Yongyan Lin <2529594L@student.gla.ac.uk>
2021-07-27 23:33:47 +0100

    chapter6 update

--- populate_rango.py
+++ populate_rango.py
@@ -6,24 +6,24 @@ from rango.models import Category,Page
 
 def populate():
     python_pages = [
-        {'title': 'Official Python Tutorial','url':'http://docs.python.org/3/tutorial/'}, 
-        {'title':'How to Think like a Computer Scientist','url':'http://www.greenteapress.com/thinkpython/'},
-        {'title':'Learn Python in 10 Minutes', 'url':'http://www.korokithakis.net/tutorials/python/'} 
+        {'title': 'Official Python Tutorial','url':'http://docs.python.org/3/tutorial/','views': 90}, 
+        {'title':'How to Think like a Computer Scientist','url':'http://www.greenteapress.com/thinkpython/','views': 84},
+        {'title':'Learn Python in 10 Minutes', 'url':'http://www.korokithakis.net/tutorials/python/','views': 40} 
     ]
     django_pages = [
-        {'title':'Official Django Tutorial', 'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'}, 
-        {'title':'Django Rocks', 'url':'http://www.djangorocks.com/'}, 
-        {'title':'How to Tango with Django', 'url':'http://www.tangowithdjango.com/'}
+        {'title':'Official Django Tutorial', 'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/','views': 93}, 
+        {'title':'Django Rocks', 'url':'http://www.djangorocks.com/','views': 23}, 
+        {'title':'How to Tango with Django', 'url':'http://www.tangowithdjango.com/','views': 35}
     ]
     other_pages = [
-        {'title':'Bottle','url':'http://bottlepy.org/docs/dev/'},
-        {'title':'Flask', 'url':'http://flask.pocoo.org'}
+        {'title':'Bottle','url':'http://bottlepy.org/docs/dev/','views': 20},
+        {'title':'Flask', 'url':'http://flask.pocoo.org','views': 68}
     ]
     cats = {'Python': {'pages': python_pages,'views' : 128, 'likes' : 64},
     'Django': {'pages': django_pages,'views' : 64, 'likes' : 32},
     'Other Frameworks': {'pages': other_pages,'views' : 32, 'likes' : 16} } 
 
-    def add_page(cat,title,url,views=0):
+    def add_page(cat,title,url,views):
         p = Page.objects.get_or_create(category=cat, title=title)[0] 
         p.url = url
         p.views = views
@@ -38,7 +38,7 @@ def populate():
     for cat, cat_data in cats.items(): 
         c = add_cat(cat,cat_data['views'],cat_data['likes']) 
         for p in cat_data['pages']: 
-            add_page(c, p['title'], p['url']) 
+            add_page(c, p['title'], p['url'],p['views']) 
     
     for c in Category.objects.all(): 
         for p in Page.objects.filter(category=c): 
--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
+from rango.models import Page
 
 def index(request): 
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     #chapter2 url = "<a href='/rango/about'>About</a>"
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {} 
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!' 
+    context_dict['categories'] = category_list
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict['pages'] = page_list
     return render(request, 'rango/index.html', context=context_dict)
 
 
@@ -11,3 +18,15 @@ def about(request):
     #chapter2 url = "<a href='/rango/'>Index</a>"
     context_dict = {'boldmessage': 'This tutorial has been put together by Yongyan Lin'}
     return render(request, 'rango/about.html', context=context_dict)
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug = category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['pages'] = None
+        context_dict['category'] = None
+    return render(request,'rango/category.html',context = context_dict)
