f4d64c154c25816922b7ad85e963decac750875b
Yongyan Lin <2529594L@student.gla.ac.uk>
2021-07-29 13:35:15 +0100

    chapter9 Exercises

--- rango/views.py
+++ rango/views.py
@@ -37,6 +37,7 @@ def show_category(request, category_name_slug):
         context_dict['category'] = None
     return render(request,'rango/category.html',context = context_dict)
 
+@login_required
 def add_category(request):
     form = CategoryForm()
     if request.method == 'POST':
@@ -48,6 +49,7 @@ def add_category(request):
             print(form.errors)
     return render(request,'rango/add_category.html',{'form':form})
 
+@login_required
 def add_page(request, category_name_slug): 
     try: 
         category = Category.objects.get(slug=category_name_slug) 
@@ -123,7 +125,7 @@ def some_view(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request,'rango/restricted.html')
 
 @login_required
 def user_logout(request):
